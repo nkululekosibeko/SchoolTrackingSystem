7259df9e5f9d81df9ec4544efc5300caa3694b5a
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-09-04 14:52:31 +0200

    Commit 3, "Added the google maps API, and it works right now."

--- app/src/main/java/com/example/schooltrackingsystem/MapsActivity.java
+++ app/src/main/java/com/example/schooltrackingsystem/MapsActivity.java
@@ -1,7 +1,6 @@
 package com.example.schooltrackingsystem;
 
 import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,14 +14,14 @@ import com.example.schooltrackingsystem.databinding.ActivityMapsBinding;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+     binding = ActivityMapsBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
