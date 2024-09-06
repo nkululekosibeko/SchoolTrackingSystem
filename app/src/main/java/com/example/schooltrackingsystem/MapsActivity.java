3f048deda7bddcc8c7f8596cdd73ed9a4447c5c7
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-09-06 21:11:52 +0200

    1. First commit
    This is the first commit from the development branch.
    
    The status of the application is that it can now create geofences, by long pressing on the map

--- app/src/main/java/com/example/schooltrackingsystem/MapsActivity.java
+++ app/src/main/java/com/example/schooltrackingsystem/MapsActivity.java
@@ -4,43 +4,36 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.Manifest;
-
-
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.schooltrackingsystem.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
     private GoogleMap mMap;
     private GeofencingClient geofencingClient;
-
+    private float GEOFENCE_RADIUS = 200;
     private int FINE_LOCATION_ACCESS_REQUEST_CODE = 10001;
-
     private ActivityMapsBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
      binding = ActivityMapsBinding.inflate(getLayoutInflater());
      setContentView(binding.getRoot());
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
         geofencingClient = LocationServices.getGeofencingClient(this);
     }
 
@@ -56,29 +49,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Durban, South Africa, and move the camera
         LatLng durban = new LatLng(-29.8587, 31.0218);
         mMap.addMarker(new MarkerOptions().position(durban).title("Marker in Durban"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(durban));
-
         enableUserLocation();
+        mMap.setOnMapLongClickListener(this);
     }
 
     private void enableUserLocation(){
         if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED){
             mMap.setMyLocationEnabled(true);
-        }else{
+        }
+        else
+        {
             //Ask for permission
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION))
+            {
                 ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_ACCESS_REQUEST_CODE);
-            }else{
+            }
+            else
+            {
                 ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_ACCESS_REQUEST_CODE);
-            // We need to show user a dialog for displaying why the permission is needed and then ask for the permission..
+                // We need to show user a dialog for displaying why the permission is needed and then ask for the permission..
+            }
         }
-        }
-
     }
 
     @Override
@@ -103,4 +99,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        addMarker(latLng);
+        addCircle(latLng, GEOFENCE_RADIUS );
+    }
+
+    private void addMarker(LatLng latLng){
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+        mMap.addMarker(markerOptions);
+    }
+
+    private void addCircle(LatLng latLng, float radius){
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(latLng);
+        circleOptions.radius(radius);
+        circleOptions.strokeColor(Color.argb(255,255,0,0));
+        circleOptions.fillColor(Color.argb(64,255,0,0));
+        circleOptions.strokeWidth(4);
+        mMap.addCircle(circleOptions);
+    }
 }
